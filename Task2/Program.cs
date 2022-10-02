2635d6bcd6acde28a8781f62ee22f01bc78fe2a9
Karina <thrasher2hockey@gmail.com>
2022-10-02 13:36:03 +0400

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.Write(a);
+}
+else
+{
+    if(a == b) //Ну, а вдруг?
+    {
+        Console.Write("Числа равны! Думал(а) попадусь? =Ь"); 
+    }
+    else
+    {
+        Console.Write(b);
+    }
+}
