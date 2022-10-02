ce260e8ce24a453fcbbe05cbb1518f27c1b8502e
Karina <thrasher2hockey@gmail.com>
2022-10-02 14:50:22 +0400

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.WriteLine("Привет, я - терминал!");
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    if(a > c)
+    {
+        Console.Write(a);
+        if(a == c)
+        {
+            Console.WriteLine("Числа ");
+            Console.WriteLine(a);
+            Console.WriteLine("и ");
+            Console.WriteLine(c);
+            Console.WriteLine(" равны. Терминал не обдуришь!");
+        }
+    }
+    else
+    {
+       Console.Write(c); 
+    }
+}
+else
+{
+    if(a == b)
+    {
+        Console.WriteLine("Числа ");
+        Console.WriteLine(a);
+        Console.WriteLine("и ");
+        Console.WriteLine(b);
+        Console.WriteLine(" равны! Ну, нельзя так мучить терминал!");
+    }
+    else
+    {
+        if(b > c)
+        {
+            Console.Write(b);
+            if(b == c)
+            {
+                Console.WriteLine("Числа ");
+                Console.WriteLine(b);
+                Console.WriteLine("и ");
+                Console.WriteLine(c);
+                Console.WriteLine(" равны... Как не стыдно!"); 
+            }
+            else
+            {
+                Console.Write(c); 
+            }
+        }
+    }
+}
+
+
