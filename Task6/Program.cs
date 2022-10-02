cd8bc643e4f4bb6bdb9ca00fdf03f347af70e264
Karina <thrasher2hockey@gmail.com>
2022-10-02 15:02:09 +0400

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int c = (a % 2);
+
+if(c == 0)
+{
+    Console.Write("Четное");
+}
+else
+{
+    Console.Write("Нечетное");
+}
