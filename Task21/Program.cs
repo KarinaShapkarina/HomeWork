54dae6cb7f806ca6c8f0df7ee36dafaa2cff2318
Karina <thrasher2hockey@gmail.com>
2022-10-11 16:58:58 +0400

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Укажи координаты 2х точек в 3D пространстве:");
+Console.WriteLine();
+bool Way = true;
+while (Way)
+{
+    try
+    {
+        Console.Write("x1= ");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("y1= ");
+        int y1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("z1= ");
+        int z1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("x2= ");
+        int x2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("y2= ");
+        int y2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("z2= ");
+        int z2 = Convert.ToInt32(Console.ReadLine());
+
+        double k = Math.Sqrt(Math.Pow(x1-x2, 2)+Math.Pow(y1-y2, 2)+Math.Pow(z1-z2,2));
+        
+        Console.WriteLine("Расстояние между точками равно: " +k);
+        
+        Way = false;
+    }
+    catch
+    {
+        Console.WriteLine("Моя твоя не понимайт. Повтори:");
+    }
+}
