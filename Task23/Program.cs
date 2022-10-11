12ac23671d3d4e362e6727da1b5bf15a9c49c220
Karina <thrasher2hockey@gmail.com>
2022-10-11 17:19:58 +0400

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Укажи число для вывода таблицы кубов:");
+Console.WriteLine();
+bool Cube = true;
+while (Cube)
+{
+    try
+    {
+        Console.Write("N = ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 1; i <= N; i++)
+        
+        Console.WriteLine("Таблица кубов: " +Math.Pow(i, 3));
+        
+        Cube = false;
+    }
+    catch
+    {
+        Console.WriteLine("Моя твоя не понимайт. Повтори:");
+    }
+}
