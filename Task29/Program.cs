4b54fa2aa987db2a4891290e8ef4dd1fdfa29031
Karina <thrasher2hockey@gmail.com>
2022-10-17 15:37:44 +0400

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите длинну массива:");
+int N =int.Parse(Console.ReadLine());
+int[] arr = new int[N];
+
+void newArray(int[]array)
+{
+    for (int i=0; i < array.Length; i++)
+     {
+        array[i] = new Random().Next(0, 10);
+     }
+}
+
+void printArray(int[]array)
+{
+    for (int i=0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < (array.Length-1)) Console.Write($", ");
+    }
+    Console.WriteLine();
+}
+
+newArray(arr);
+printArray(arr);
