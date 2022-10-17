9897888fe2739bddec160d5c11405063d9028edb
Karina <thrasher2hockey@gmail.com>
2022-10-17 14:16:45 +0400

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Нахождение суммы чисел в заданом числе.");
+
+int Message(string message)
+{
+   System.Console.WriteLine(message);
+   string resultRead = System.Console.ReadLine();
+   int result = int.Parse(resultRead);
+   return result;
+}
+
+int number = Message("Укажи число:");
+
+int SumOfNuber(int number)
+{
+int sum = 0;
+while (number > 0)
+     {
+        sum += number % 10;
+        number = number / 10;
+     }
+      return sum;
+}
+
+System.Console.WriteLine($"Результат {SumOfNuber(number)}");
