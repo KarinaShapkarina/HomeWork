125db5e3cc3b6792d9494da3373c5d60adc8fbad
Karina <thrasher2hockey@gmail.com>
2022-10-17 11:25:49 +0400

    Task15

---
+++ Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Возведение числа A в степень B.");
+
+int Message(string message)
+{
+   System.Console.WriteLine(message);
+   string resultRead = System.Console.ReadLine();
+   int result = int.Parse(resultRead);
+   return result;
+}
+
+int a = Message("Укажи число А:");
+int b = Message("Укажи степень B:");
+
+int Cycle(int a, int b)
+{
+int c = 1;
+for (int i = 1; i <= b; i++)
+     {
+        c *= a;
+     }
+      return c;
+}
+
+System.Console.WriteLine($"Результат {Cycle(a,b)}");
