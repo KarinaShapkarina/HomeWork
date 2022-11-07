af76419809aeeefae27d8769e12cc77f0b14e5f9
Karina <thrasher2hockey@gmail.com>
2022-11-07 16:47:02 +0400

    Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.Clear();
+// Console.WriteLine($"Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
+// Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+// int m = InputNumbers("Введите m: ");
+// int n = InputNumbers("Введите n: ");
+// int range = InputNumbers("Введите диапазон: от 1 до ");
+Console.Write("Введи количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введи количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+void FillArray(int[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      matr[i, j] = new Random().Next(1,10);;
+    }
+  }
+}
+
+void Print(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+Console.WriteLine($"\nРандомный массив: ");
+FillArray(array);
+Print(array);
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+Print(array);
