77bd558f359a220373d0e0943d4f3ea100c4b6dc
Karina <thrasher2hockey@gmail.com>
2022-11-07 18:36:22 +0400

    Task56

---
+++ Task56/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.Clear();
+Console.Write("Введи количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введи количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+void FillArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1,10);;
+    }
+  }
+}
+
+void Print(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+Console.WriteLine($"\nРандомный массив: ");
+FillArray(array);
+Print(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой равной {sumLine}");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
