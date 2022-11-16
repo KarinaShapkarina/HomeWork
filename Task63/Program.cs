4f6f58fbad7a984748e60700df76431c63224e56
Karina <thrasher2hockey@gmail.com>
2022-11-16 18:06:25 +0400

    Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введи число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+printNaturalNumbers(number);
+
+void printNaturalNumbers(int n)
+{
+   int number = 1;
+    if(n == number)
+    {
+      Console.Write(n);  
+    }
+    else 
+    {
+      Console.Write(n);  
+      printNaturalNumbers(n - 1);
+    }   
+}
