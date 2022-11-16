4dbc0545f3e7f77cda37ceae51842009ab78f4e1
Karina <thrasher2hockey@gmail.com>
2022-11-16 18:20:07 +0400

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введи число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов = {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
